703bad41e1e6f39ac97e86c7cb5b0817db443d12
Charles-Chiang <charlie.c.chiang@gmail.com>
2022-06-25 17:15:11 -0400

    Problem 1 improved

--- Problem1.cpp
+++ Problem1.cpp
@@ -6,8 +6,8 @@
 
 #include <iostream>
 
-int main()
-{
+//method 1 - the CS major
+int method1 () {
     int sum = 0;
     for (int i = 1; i < 1000; i++) {
         if (i%15 == 0) {
@@ -18,8 +18,30 @@ int main()
             sum += i;
         }
     }
-    std::cout << sum << std::endl;
-    return 0;
+    return sum;
+}
+
+//method 2 - the math major (math you can do on a piece of paper) (maybe with a calculator)
+int method2 () {
+    int sum;
+    int mod15 = 999/15;
+    int mod3 = 999/3;
+    int mod5 = 999/5;
+
+    int sum3s = (1+mod3)*mod3/2;
+    int sum5s = (1+mod5)*mod5/2;
+    int sum15s = (1+mod15)*mod15/2;
+
+    sum = sum3s*3 + sum5s*5 - sum15s*15;
+    return sum;
+}
+
+int main()
+{
+    std::cout << method1() << std::endl;
+
+    std::cout << method2() << std::endl;
+
 }
 
 // Answer: 233168
